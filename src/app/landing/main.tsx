9d90fa64f37c76402a596140df447f59e692e75f
Syxtyz <lordgraffin@gmail.com>
2025-12-14 23:07:16 +0800

    Feat: Added Landing Page

--- src/app/landing/main.tsx
+++ src/app/landing/main.tsx
@@ -1,7 +1,15 @@
 export default function LandingContent() {
     return (
-        <main id="landing" className="rounded bg-blue-500">
-            <div>Landing Page</div>
+        <main id="landing" className="rounded h-screen flex items-center">
+            <div className="w-1/2 ml-32 grid gap-4">
+                <p className="text-6xl font-loto">Improving Chat-Support experiences</p>
+                <span className="text-2xl flex text-nowrap gap-1.5">
+                    <p>Eduardo Lagumbay</p>
+                    <p className="text-muted-foreground">is a</p>
+                    <p>Software developer</p>
+                    <p className="text-muted-foreground">focusing in desktop applications</p>
+                </span>
+            </div>
         </main>
     )
 }
